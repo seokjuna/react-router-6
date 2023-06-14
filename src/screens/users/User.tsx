5170f7aea374948f0fbc1b1bccb26b002333e2f4
naseokju <lukas95@naver.com>
2023-06-14 17:40:24 +0900

    Outlet

--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { users } from "../../db";
+import { Outlet } from "react-router-dom";
 
 function User() {
     const { userId } = useParams();
     return (
-        <h1>
-            User with id {userId} is named: {users[Number(userId) - 1].name}
-        </h1>
+        <div>
+            <h1>
+                User with id {userId} is named: {users[Number(userId) - 1].name}
+            </h1>
+            <hr />
+            <Link to="followers">See Followers</Link> 
+            <Outlet />
+        </div>
     );
 }
 
